d68928058de658893a9e8ef7aea3fea2858d81f8
Aleix Oraa <aoraa1@iesebre.com>
2024-03-12 16:12:55 +0100

    model missatge

--- odoo-tutorial/technical-training/reclamacions/models/reclamacio.py
+++ odoo-tutorial/technical-training/reclamacions/models/reclamacio.py
@@ -6,3 +6,6 @@ class Reclamacio(models.Model):
     client_id = fields.Many2one('res.partner', string='Client que reclama')
     creator_id = fields.Many2one('res.users', string='Usuari que crea reclamació', default=lambda self: self.env.user.id)
     creation_date = fields.Date('Data de creació', copy=False, default=lambda self: datetime.today().date())
+    modification_date = fields.Date('Data de modificació', copy=False, default=lambda self: datetime.today().date())
+    closing_date = fields.Date('Data de tancament', copy=False, default=lambda self: datetime.today().date())
+    
